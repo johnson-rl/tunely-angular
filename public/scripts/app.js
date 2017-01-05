a9610021393d93c970974794da74f99ad43772a3
johnson-rl <johnson.rl@gmail.com>
2017-01-05 15:46:40 -0800

    connected get all albums to db

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,27 +11,43 @@ angular
   // the second argument is a function that defines the capacities
   // of the controller.
 
-function AlbumsIndexController () {
+AlbumsIndexController.$inject = ['$http'];
+
+function AlbumsIndexController ($http) {
   var vm = this;
   vm.newAlbum = {};
 
-  vm.newAlbum = {
-      name: 'Viva Hate',
-      artistName: 'Morrissey'
-  };
+  $http({
+    method: 'GET',
+    url: '/api/albums'
+  }).then(gotAlbums,dinnaGetAlbums)
 
-  vm.albums = [
-    {
-      name: 'Coming Home',
-      artistName: 'Leon Bridges'
-    },
-    {
-      name: 'Are We There',
-      artistName: 'Sharon Van Etten'
-    },
-    {
-      name: 'The Queen is Dead',
-      artistName: 'The Smiths'
-    }
-  ];
+  function gotAlbums(res){
+    console.log(res)
+    vm.albums = res.data
+  }
+
+  function dinnaGetAlbums(err){
+    console.log('Ahhh, schnikes....dinna get albums', err)
+  }
+
+  // vm.newAlbum = {
+  //     name: 'Viva Hate',
+  //     artistName: 'Morrissey'
+  // };
+  //
+  // vm.albums = [
+  //   {
+  //     name: 'Coming Home',
+  //     artistName: 'Leon Bridges'
+  //   },
+  //   {
+  //     name: 'Are We There',
+  //     artistName: 'Sharon Van Etten'
+  //   },
+  //   {
+  //     name: 'The Queen is Dead',
+  //     artistName: 'The Smiths'
+  //   }
+  // ];
 }
