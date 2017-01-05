1906558c1a9ad1b699bc78e62d6c1588a76d16b8
johnson-rl <johnson.rl@gmail.com>
2017-01-05 13:15:32 -0800

    sprint 1 complete

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,4 +10,18 @@ function AlbumsIndexController () {
       name: 'License to Ill',
       artistName: 'Beastie Boys'
   };
+  vm.albums = [
+  {
+    name: 'Coming Home',
+    artistName: 'Leon Bridges'
+  },
+  {
+    name: 'Are We There',
+    artistName: 'Sharon Van Etten'
+  },
+  {
+    name: 'The Queen is Dead',
+    artistName: 'The Smiths'
+  }
+];
 }
